a6de8351c3b3ec996ecd7fe973da8eda3707b9ba
akshay.gupta <akshaykumargupta2208@gmail.com>
2023-09-13 15:26:51 +0530

    simple job added

--- examples/dynamic-jobs/src/main.groovy
+++ examples/dynamic-jobs/src/main.groovy
@@ -1,7 +1,5 @@
-@Grab('org.yaml:snakeyaml:1.17')
-import org.yaml.snakeyaml.Yaml
 import hudson.model.*
-
+import utils.JobUtils
 
 // function to search for all the files ending with .yaml or .yml
 ArrayList searchYamlFiles(String dirPath) {
@@ -28,11 +26,11 @@ pipeline_file_list  = searchYamlFiles(cwd.toString())
 for (current_pipeline in pipeline_file_list){
 
     println("Working on :"+current_pipeline)
-    // parsing the yaml content
-    parsed_job_config = new Yaml().load((current_pipeline as File).text)
 
-    println("jobname is : "+parsed_job_config.job_name)
-    job(parsed_job_config.job_name) {
+    JobUtils job_config = new JobUtils(current_pipeline)
+
+    println("jobname is : "+job_config.job_name)
+    job(job_config.job_name) {
         steps {
             shell('echo Hello World!')
         }
